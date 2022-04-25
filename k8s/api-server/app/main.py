ee665c99f730cefe53b2786449f6e59863e6a899
Allen Clarence <zombiedelah@gmail.com>
2022-04-25 18:49:43 +0900

    added server side logic

--- k8s/api-server/app/main.py
+++ k8s/api-server/app/main.py
@@ -1,14 +1,66 @@
 from typing import Optional
 from fastapi import FastAPI
+from fastapi import WebSocket, WebSocketDisconnect
+from pydantic import BaseModel
+import nats
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.connections = {}
+
+    async def onConnection(self, websocket: WebSocket, uid: str):
+        if uid in self.connections:
+            print("uid: {id} exists.".format(id=uid))
+        else:
+            await websocket.accept()
+            self.connections[uid] = websocket
+            print("-" * 50)
+            print("A new connection has been made")
+            print("uid: {id}".format(id=uid))
+            print("socket: {sock}".format(sock=self.connections[uid]))
+
+    def onDisconnection(self, uid: str):
+        print("-" * 50)
+        print("A client disconnected")
+        print("uid: {id}".format(id=uid))
+        print("socket: {sock}".format(sock=self.connections[uid]))
+        del self.connections[uid]
+
+    async def publish(self, uid: str):
+        data = self.connections[uid].receive_json()
+        print(data)
+
+
+class MinimalData(BaseModel):
+    index: int
+    value0: Optional[list] = None
+    value1: Optional[str] = None
+    value2: Optional[int] = None
+    value3: Optional[float] = None
+
 
 app = FastAPI()
+print("Starting the api server.")
+manager = ConnectionManager()
+print("Initializing the connection manager.")
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"websocket uri": "/stream/$uid"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/post/")
+async def minimalPublish(data: MinimalData):
+    return data
+
+
+@app.websocket("/websocket/{uid}")
+async def streamPublish(websocket: WebSocket, uid: str):
+    await manager.onConnection(websocket, uid)
+    try:
+        while True:
+            await manager.publish(uid)
+    except WebSocketDisconnect:
+        manager.onDisconnection(uid)
