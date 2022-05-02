dbcf50b5c7ac4e19b92aa9593aaa80f446c09263
Happydj <56625382+PARKDONGJAE@users.noreply.github.com>
2022-05-02 10:22:10 +0900

    Update make_namelist.py

--- crawler/make_namelist.py
+++ crawler/make_namelist.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 import time
-
+import traceback
 sub_list = ['까치산',  '시청',  '을지로입구', '을지로3가', '을지로4가', '동대문역사문화공원', '신당', '상왕십리', '왕십리',  '한양대',  '뚝섬', '성수', '건대입구',
              '구의', '강변', '잠실나루', '잠실', '잠실새내', '종합운동장', '삼성', '선릉', '역삼', '강남', '교대', '서초', '방배', '사당', '낙성대', '서울대입구',
             '봉천', '신림', '신대방', '구로디지털단지', '대림', '신도림', '문래', '영등포구청', '당산', '합정', '홍대입구', '신촌', '이대', '아현', '충정로', '용답',
@@ -75,11 +75,13 @@ def get_data(name):
     # 없는 페이지면
     except:
         driver.quit()
+        print(traceback.format_exc())
+
         return name_list
                 
             
             
-            
+    # 다음페이지 7번 하게 되면(최대)        
     driver.quit()
     return name_list
     
