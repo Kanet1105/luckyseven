7079e6e1eb5c03ce9d47263468ebb62c3a297c44
nyongja <nyongja@naver.com>
2022-05-04 22:30:38 +0900

    send place info

--- crawler/common/network.py
+++ crawler/common/network.py
@@ -1,10 +1,30 @@
 import requests
 
+HOST = 'http://127.0.0.1:8000/data'
 
-def send_data():
+class Payload:
+    placeInfo = {
+        'placeName': None ,
+        'placeType': None,
+        'placeAddress': None,
+        'latitude': None,
+        'longitude': None,
+        'telephone': None,
+        'description': None,
+        'menu': dict(),
+        'themeKeywords': [],
+        'agePopularity': dict(),
+        'genderPopularity': dict(),
+        'time': dict(),
+        'placeMeanRating': None,
+        'visitReviewNum': None,
+        'blogReviewNum': None,
+        'like': dict(),
+    }
+
+def sendData(kind: str, data: dict):
     # requests.post 로 데이터 전송
-
-    ########
-    # fill #
-    ########
+    if kind == "placeInfo":
+        result = requests.post(url=HOST, json=data)
+        print(result)
     pass
