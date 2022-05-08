2c2d8d9fe7ea68744d9b397a5226365caf124729
nyongja <nyongja@naver.com>
2022-05-08 21:02:08 +0900

    network update

--- crawler/common/network.py
+++ crawler/common/network.py
@@ -4,52 +4,54 @@ HOST = 'http://127.0.0.1:8000/{uri}'
 
 
 class Payload:
-    placeInfo = {
-        'placeName': None ,
-        'placeType': None,
-        'placeAddress': None,
-        'latitude': None,
-        'longitude': None,
-        'telephone': None,
-        'description': None,
-        'menu': dict(),
-        'themeKeywords': [],
-        'agePopularity': dict(),
-        'genderPopularity': dict(),
-        'time': dict(),
-        'placeMeanRating': None,
-        'visitReviewNum': None,
-        'blogReviewNum': None,
-        'like': dict(),
-    }
+    def __init__(self):
+        self.placeInfo = {
+            'placeName': "",
+            'placeType': "",
+            'placeAddress': "",
+            'latitude': 0.0,
+            'longitude': 0.0,
+            'telephone': "",
+            'description': "",
+            'menu': dict(),
+            'themeKeywords': [],
+            'agePopularity': dict(),
+            'genderPopularity': dict(),
+            'time': dict(),
+            'placeMeanRating': 0.0,
+            'visitReviewNum': 0,
+            'blogReviewNum': 0,
+            'like': dict(),
+        }
 
-    reviewInfo = {
-        'userHash': None,
-        'reviewUserID': None,
-        'placeName': None,
-        'placeAddress': None,
-        'reviewContent': None,
-        'reviewInfoScore': None,
-        'reviewInfoVisitDay': None,
-        'reviewInfoVisitCount': None,
-    }
+        self.reviewInfo = {
+            'userHash': None,
+            'reviewUserID': None,
+            'placeName': None,
+            'placeAddress': None,
+            'reviewContent': None,
+            'reviewInfoScore': None,
+            'reviewInfoVisitDay': None,
+            'reviewInfoVisitCount': None,
+        }
 
-    userInfo = {
-        'userHash': None,
-        'userID': None,
-        'reviewNum': None,
-        'photo': None,
-        'following': None,
-        'follower': None,
-    }
+        self.userInfo = {
+            'userHash': None,
+            'userID': None,
+            'reviewNum': None,
+            'photo': None,
+            'following': None,
+            'follower': None,
+        }
 
-    placeNameInfo = {
-        'placeName': None,
-        'placeAddress': None
-    }
+        self.placeNameInfo = {
+            'placeName': None,
+            'placeAddress': None
+        }
 
 
 def sendData(kind: str, data: dict):
     # requests.post 로 데이터 전송
     result = requests.post(url=HOST.format(uri=kind), json=data)
     print(result)
+
