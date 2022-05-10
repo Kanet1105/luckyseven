368d2330aea7066332eb563fcad6514155ec691a
Allen Clarence <zombiedelah@gmail.com>
2022-05-10 21:19:53 +0900

    implemented payload agnostic writes to the Nats server

--- api-server/main.py
+++ api-server/main.py
@@ -1,20 +1,33 @@
-from templates import schema
+"""
+The API server handle payload agnostic writes to the Nats Jetstream Server
+"""
+
 from fastapi import FastAPI
+import pickle
+from publisher import Publisher
+from templates import schema
 
-
+HOST = '{address}:{port}'
 app = FastAPI()
+pub = Publisher(HOST)
+
+
+async def publish(data, subject, timeout, stream):
+    payload = pickle.dumps(data.dict())
+    ack = await pub.publish(subject, payload, timeout, stream)
+    return ack
 
 
 @app.post('/PlaceInfoModel')
 async def receivePlaceInfo(data: schema.PlaceInfoModel):
-    print(data)
+    return await publish(data, 'scraped', 2.0, 'data')
 
 
 @app.post('/ReviewInfoModel')
 async def receiveReviewInfo(data: schema.ReviewInfoModel):
-    print(data)
+    return await publish(data, 'scraped', 2.0, 'data')
 
 
 @app.post('/UserInfoModel')
 async def receiveUserInfo(data: schema.UserInfoModel):
-    print(data)
+    return await publish(data, 'scraped', 2.0, 'data')
